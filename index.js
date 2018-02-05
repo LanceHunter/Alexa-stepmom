859a79180ffb1839acd795f95e4ce45d6c21c618
Lance Hunter <lancehunter@gmail.com>
2018-02-05 16:31:01 -0600

    Added checking for speechcons.

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const greetings = [
   `I'm here, ready to do what you can't.`
 ];
 
+// Things to say for unhandled requests.
+const confusion = [
+  `You're not making any sense.`,
+  `I have no idea what you're talking about.`,
+  `I would ask you to say that again, but I'd prefer you didn't.`
+];
+
 const farewells = [
   `Well that's just perfect. Goodbye.`,
   `I don't care for your tone. Goodbye.`,
@@ -29,28 +36,34 @@ const handlers = {
   // First handler, for dealing with skill launch.
   'LaunchRequest' :  function() {
       let greetReply = greetings[(Math.floor(Math.random() * greetings.length))];
-      this.response.speak(greetReply);
+      this.response.speak(addSpeehconSSML(greetReply));
       this.emit(`:responseReady`);
   },
 
 
   // Stop
-  'AMAZON.StopIntent': function() {
+  'AMAZON.StopIntent' : function() {
       let farewellReply = farewells[(Math.floor(Math.random() * farewells.length))];
-      this.response.speak(farewellReply);
+      this.response.speak(addSpeehconSSML(farewellReply));
       this.emit(':responseReady');
   },
 
   // Cancel
-  'AMAZON.CancelIntent': function() {
+  'AMAZON.CancelIntent' : function() {
       let farewellReply = farewells[(Math.floor(Math.random() * farewells.length))];
-      this.response.speak(farewellReply);
+      this.response.speak(addSpeehconSSML(farewellReply));
       this.emit(':responseReady');
   },
 
-  'SessionEndedRequest': function() {
+  'SessionEndedRequest' : function() {
 //    console.log('session ended!');
 //    this.emit(':saveState', true);
+  },
+
+  'Unhandled' : function() {
+      let confusionReply = confusion[(Math.floor(Math.random() * confusion.length))];
+      this.response.speak(addSpeehconSSML(confusionReply));
+      this.emit(':responseReady');
   }
 
 };
@@ -61,3 +74,45 @@ exports.handler = function(event, context, callback){
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
+
+// Functions outside of the handlers.
+
+
+// This function takes the text and checks for Speechcons, adding appropriate SSML as necessary.
+function addSpeehconSSML (text) {
+  speechcons.forEach(function(element){
+    var elementWithSSML=',<say-as interpret-as="interjection">'+element+'</say-as>,';
+    text = text.replace(element,elementWithSSML);
+  });
+  console.log(text);
+  return text;
+}
+
+
+
+// List of valid speechcons
+const speechcons = ["abracadabra","achoo","aha","ahem","ahoy","all righty","aloha",
+"aooga","argh","arrivederci","as you wish","au revoir","aw man","baa",
+"bada bing bada boom","bah humbug","bam","bang","batter up","bazinga",
+"beep beep","bingo","blah","blarg","blast","boing","bon appetit","bonjour",
+"bon voyage","boo","boo hoo","boom","booya","bravo","bummer","caw","cha ching",
+"checkmate","cheerio","cheers","cheer up","chirp","choo choo","clank",
+"click clack","cock a doodle doo","coo","cowabunga","darn","ding dong","ditto",
+"d’oh","dot dot dot","duh","dum","dun dun dun","dynomite","eek","eep",
+"encore","en gard","eureka","fancy that","geronimo","giddy up","good grief",
+"good luck","good riddance","gotcha","great scott","heads up","hear hear",
+"hip hip hooray","hiss","honk","howdy","hurrah","hurray","huzzah","jeepers creepers",
+"jiminy cricket","jinx","just kidding","kaboom","kablam","kaching","kapow",
+"katchow","kazaam","kerbam","kerboom","kerching","kerchoo","kerflop",
+"kerplop","kerplunk","kerpow","kersplat","kerthump","knock knock","le sigh",
+"look out","mamma mia","man overboard","mazel tov","meow","merci","moo",
+"nanu nanu","neener neener","no way","now now","oh boy","oh brother","oh dear",
+"oh my","oh snap","oink","okey dokey","oof","ooh la la","open sesame","ouch",
+"oy","phew","phooey","ping","plop","poof","pop","pow","quack","read ‘em and weep",
+"ribbit","righto","roger","ruh roh","shucks","splash","spoiler alert","squee",
+"swish","swoosh","ta da","ta ta","tee hee","there there","thump","tick tick tick",
+"tick-tock","touche","tsk tsk","tweet","uh huh","uh oh","voila","vroom",
+"wahoo","wah wah","watch out","way to go","well done","well well","wham",
+"whammo","whee","whew","woof","whoops a daisy","whoosh","woo hoo","wow",
+"wowza","wowzer","yadda yadda yadda","yay","yikes","yippee","yoink","yoo hoo",
+"you bet","yowza","yowzer","yuck","yum","zap","zing","zoinks",];
